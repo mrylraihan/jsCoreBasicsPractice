cc7811621f5939cb7608621f0caacdb6b57cb85d
mrylraihan <mr.ylraihan@gmail.com>
2021-08-01 18:50:01 -0400

    refreshing jscript

---
+++ JsCoreBasics2/Section2/basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,9 @@
+const defautResult = 0;
+
+let currentResult = defautResult;
+
+currentResult = currentResult + 10;
+
+let calculationDescription = `${currentResult} + 5 + 5`;
+
+outputResult(currentResult, calculationDescription);
