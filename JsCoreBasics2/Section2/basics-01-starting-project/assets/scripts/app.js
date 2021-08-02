29e45293e82dc8ca251d60d1b35c4f92146b3367
Wayel Raihan <mr.ylraihan@gmail.com>
2021-08-02 18:06:34 -0400

    more practice

--- JsCoreBasics2/Section2/basics-01-starting-project/assets/scripts/app.js
+++ JsCoreBasics2/Section2/basics-01-starting-project/assets/scripts/app.js
@@ -1,10 +1,23 @@
+//how to create a function in Js
+// function greetUser(name){
+// console.log(`hey user your name is ${name} am i right`)
+// }
+
+// greetUser('Wayel');
+
+//these variables have global scope 
 const defautResult = 0;
 
 let currentResult = defautResult;
 
-currentResult = currentResult + 10;
+function add(num1, num2){
+    const result = num1 + num2;
+    return result;
+}
 
-let calculationDescription = `${currentResult} + 5 + 5`;
+currentResult = add(5, 9);
+
+let calculationDescription = `${defautResult} + 5 + 5`;
 
 outputResult(currentResult, calculationDescription);
 
