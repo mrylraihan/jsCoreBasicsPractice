42edf40c97911cdc0d5ab3701f3c0cc7d3bafffd
mrylraihan <mr.ylraihan@gmail.com>
2021-08-04 16:44:13 -0400

    renaming files

---
+++ JsCoreBasics2/Section2/basics-01.5-calculator-project-with-arrays/assets/scripts/app.js
@@ -0,0 +1,46 @@
+const defautResult = 0;
+let currentResult = defautResult;
+
+function getUserNumberInput(){
+    return parseInt(usrInput.value)
+}
+
+function createAndWriteOutput(operator , resultBeforeCalc , calcNumber){
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription);
+}
+
+function add(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult += enteredNumber;
+    createAndWriteOutput('+',initialResult, currentResult);
+}
+
+function subtract(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult -= enteredNumber;
+    createAndWriteOutput('-',initialResult, currentResult);
+}
+
+function multiply(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult *= enteredNumber;
+    createAndWriteOutput('*',initialResult, currentResult);
+}
+
+function divide(){
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult /= enteredNumber;
+    createAndWriteOutput('/',initialResult, currentResult);
+}
+
+
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
