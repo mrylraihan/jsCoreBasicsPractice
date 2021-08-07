197fae590d61e7f963aa072ab4936c276528cc27
mrylraihan <mr.ylraihan@gmail.com>
2021-08-07 11:50:36 -0400

    personal assignment organized

--- JsCoreBasics2/PersonalAssignment/eventlistenerPractice/myapp.js
+++ JsCoreBasics2/PersonalAssignment/eventlistenerPractice/myapp.js
@@ -1,11 +1,32 @@
+const redp = document.getElementById('p-tag');
+redp.onclick = ()=> alert('this is a red p-tag');
+
+
+const yellowDiv = document.getElementById('yellow');
+yellowDiv.onclick = function logColor(){
+    alert('this is a yellow div tag');
+};
+
+
+const greenDiv = document.getElementById('green');
+greenDiv.onclick = function alertColor(){
+    alert('this is a green div tag');
+};
+
+
+const brownElmt = document.getElementById('brown');
+brownElmt.style.cssText = "width:200px; height:200px; background:brown;";
+const callingBrown = ()=> alert('brown is here!');
+brownElmt.addEventListener('click', callingBrown);
+
+
 const btn = document.getElementById("btn");
-
 const buttonPress = ()=> alert("hey");
-
 btn.addEventListener("click", buttonPress);
 
+
 const anBtn = document.getElementById("anBtn");
-
 const anotherButton = ()=> alert("another button press");
-
 anBtn.addEventListener("click", anotherButton);
+
+
