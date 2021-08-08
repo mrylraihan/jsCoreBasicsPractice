e0dd53c568b8c5e3ba19880e5b1be0ee09080fe7
mrylraihan <mr.ylraihan@gmail.com>
2021-08-08 14:06:40 -0400

    mouseover and mouse out in app3 and index3

--- JsCoreBasics2/PersonalAssignment/ComparisonAndConditonals/app3.js
+++ JsCoreBasics2/PersonalAssignment/ComparisonAndConditonals/app3.js
@@ -25,3 +25,8 @@ for(let i = 0; i<classRegister.length;i++){
 for(let i = classRegister.length-1; 0<=i; i--){//inverted list
     console.log(classRegister[i]);
 }
+
+const hOne = document.getElementById('hOne');
+const originalText = hOne.textContent;// gets the original text stored in the h1 and stores it in the new variable
+hOne.onmouseover = () => hOne.innerHTML = 'Condtionals and Looping practice';// changes the html onhover
+hOne.onmouseout = () => hOne.innerHTML = `${originalText}`;// on mouse out returns the original text that was stored in hOne1
