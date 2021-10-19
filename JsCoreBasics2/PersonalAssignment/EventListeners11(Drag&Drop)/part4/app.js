b34daa9d1c390b360e167857db3d1c655c97ae2c
mrylraihan <mr.ylraihan@gmail.com>
2021-10-19 16:32:12 -0400

    drag and drop

---
+++ JsCoreBasics2/PersonalAssignment/EventListeners11(Drag&Drop)/part4/app.js
@@ -0,0 +1,37 @@
+const swimmer = document.getElementById('swimmer')
+
+
+function draggingElements(e) {
+	e.dataTransfer.setData('text/plain', this.id)
+	console.log(this)
+	console.log(e)
+}
+
+swimmer.addEventListener('dragstart', draggingElements)
+
+
+const divs = document.querySelectorAll('.dropZone')
+
+for (const div of divs) {
+	//when a draggable element is over a zone
+	div.addEventListener('dragover', (e) => {
+		e.preventDefault()
+		div.classList.add('drag-over')
+		console.log(e)
+	})
+
+	div.addEventListener('dragleave', (e) => {
+		console.log(e)
+		div.classList.remove('drag-over')
+	})
+
+	//when a draggable element is in a drop zone
+	div.addEventListener('drop', (e) => {
+		e.preventDefault()
+		const dropElementId = e.dataTransfer.getData('text/plain')
+		const dropElement = document.getElementById(dropElementId)
+		div.appendChild(dropElement)
+		div.classList.remove('drag-over')
+		console.log(e)
+	})
+}
