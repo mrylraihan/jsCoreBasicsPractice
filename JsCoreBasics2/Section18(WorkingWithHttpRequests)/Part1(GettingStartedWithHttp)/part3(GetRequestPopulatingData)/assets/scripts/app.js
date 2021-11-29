3082f27f2f74e132dd71e954c628a41c6846f8da
mrylraihan <mr.ylraihan@gmail.com>
2021-11-29 17:46:32 -0500

    more http request

---
+++ JsCoreBasics2/Section18(WorkingWithHttpRequests)/Part1(GettingStartedWithHttp)/part3(GetRequestPopulatingData)/assets/scripts/app.js
@@ -0,0 +1,25 @@
+// https://jsonplaceholder.typicode.com/
+
+const xhr = new XMLHttpRequest();
+
+
+ xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts')
+
+ xhr.responseType = 'json'
+ xhr.onload = function () {
+    // const listOfPosts = JSON.parse(xhr.response)
+    const listOfPosts = xhr.response
+    console.log(listOfPosts);
+    for (const post of listOfPosts) {
+        const postEl = document.importNode(postTemplate.content, true)
+        postEl.querySelector('h2').textContent = post.title.toUpperCase()
+        postEl.querySelector('p').textContent = post.body
+        listElement.append(postEl)
+    }
+    
+ }
+
+ xhr.send();
+
+ const listElement = document.querySelector('.posts')
+ const postTemplate = document.getElementById('single-post')
