2f2e4e7877757d47c72a3e42a601529ac24b2492
mrylraihan <mr.ylraihan@gmail.com>
2022-01-11 12:40:34 -0500

    justincase

---
+++ JsCoreBasics2/PersonalAssignment/GAPracticeStuff/OutcomesPractice/app.js
@@ -0,0 +1,13 @@
+const DUMMY_EXPENSES = [
+    { id: 'e1', title: 'Toilet Paper', amount: 294.67, date: new Date(2021, 9, 11) },
+    { id: 'e2', title: 'New TV', amount: 314.99, date: new Date(2021, 1, 10) },
+    { id: 'e3', title: 'Car Insurance', amount: 56.76, date: new Date(2020, 2, 18) },
+    { id: 'e4', title: 'New Sit/Stand Desk(Ebony Wood)', amount: 235.36, date: new Date(2021, 2, 28) }
+]
+
+
+const filteredList = DUMMY_EXPENSES.filter(item=>{
+    return item.date.getFullYear() === 2021
+})
+
+console.log(filteredList);
