8a2f5d5462fc6c1f3a48d159789b9c124166a8ad
mrylraihan <mr.ylraihan@gmail.com>
2022-02-08 20:22:14 -0500

    Reversing String practice

---
+++ JsCoreBasics2/PersonalAssignment/SumClosestToZero/ReverseString/app2.js
@@ -0,0 +1,34 @@
+// First way
+const word = 'word'
+const rWord = (w) => {
+    let arrW = w.split('').reverse().join('')
+    console.log(arrW)
+}
+
+rWord(word)
+
+
+// Second way 
+function reverseString2(str) {
+    let result = ''
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        result += str[i]
+    }
+
+    return result
+}
+
+console.log(reverseString2('cat'))
+
+// third way
+const reverseWord3 = (w) => {
+    w = Array.from(w)
+    let rWord = ''
+    for (let i = 0; 0 < w.length; i++) {
+        rWord += w.pop()
+    }
+    return rWord
+}
+
+console.log(reverseWord3(word))
