4cc15d8b3631ec31cbe1f0d8d0b4753e81bb0503
mrylraihan <mr.ylraihan@gmail.com>
2022-04-22 09:27:38 -0700

    created all Crud methods for my Gundam backend with fetch api

--- JsCoreBasics2/PersonalAssignment/FetchCrud/app.js
+++ JsCoreBasics2/PersonalAssignment/FetchCrud/app.js
@@ -1,11 +1,14 @@
 const btn1 = document.getElementById('btn1')
+const btn1_5 = document.getElementById('btn1_5')
 const btn2 = document.getElementById('btn2')
 const btn3 = document.getElementById('btn3')
 const btn4 = document.getElementById('btn4')
+const btn5 = document.getElementById('btn5')
 
-const url = ''
+const url = 'http://localhost:4000/gundams/'
 const editId = 1
-const deletedId = 3
+const deletedId = 5
+const patchId = 4
 
 
 const fetchAll = ()=>{
@@ -16,9 +19,19 @@ const fetchAll = ()=>{
             console.log(result);
         }).catch(err=>console.log(err))
 }
+btn1.addEventListener('click', fetchAll)
+const getOneById = ()=>{
+    const res = fetch(url+editId)
+    console.log(res)
+    res.then(res=>res.json())
+        .then(result=>{
+            console.log(result);
+        }).catch(err=>console.log(err))
+}
+btn1_5.addEventListener('click', getOneById)
 
-function createPerson(){
-    const data = { name : 'Heshow' };
+function createGundam(){
+    const data = { pilot : 'Duo', gundam:'DeathSyth' };
 
     return fetch(url, {
         method: 'POST',
@@ -30,29 +43,70 @@ function createPerson(){
     .then(data=>console.log("Success: ", data))
     .catch(error=>console.log("Error: ", error))
 }
+btn2.addEventListener('click', createGundam)
 
-function editPerson(){
-    const data = { name: 'Wayel' };
-
-   return fetch(url + editId, {
+function editWholeGundam(){
+    const data = { pilot: 'Wayel Raihan', gundam:'Mars' };
+    
+    return fetch(url + editId, {
         method: 'PUT',
         headers: {
             "Content-Type": "application/json",
         },
         body: JSON.stringify(data),
     }).then(res => res.json())
-        .then(data => console.log("Success: ", data))
-        .catch(error => console.log("Error: ", error))
+    .then(data => console.log("Success: ", data))
+    .catch(error => console.log("Error: ", error))
 }
-function deletePerson(){
+btn3.addEventListener('click', editWholeGundam)
 
-    return  fetch(url + deletedId, {
-        method: 'POST',
+function editGundam(){
+    const data = { pilot: 'Duo Maxwell' };
+    
+    return fetch(url + patchId, {
+        method: 'PATCH',
         headers: {
             "Content-Type": "application/json",
         },
         body: JSON.stringify(data),
     }).then(res => res.json())
-        .then(data => console.log("Success: ", data))
-        .catch(error => console.log("Error: ", error))
+    .then(data => console.log("Success: ", data))
+    .catch(error => console.log("Error: ", error))
 }
+btn4.addEventListener('click', editGundam)
+
+function deleteGundam(){
+    
+    return  fetch(url + deletedId, {
+        method: 'DELETE',
+        headers: {
+            "Content-Type": "application/json",
+        }
+    }).then(res => res.json())
+    .then(data => console.log("Success: ", data))
+    .catch(error => console.log("Error: ", error))
+}
+
+btn5.addEventListener('click', deleteGundam)
+
+const btn6 = document.getElementById('btn6')
+const btn7 = document.getElementById('btn7')
+
+const fetchAllGundams = () => {
+    const res = fetch(url+'gundam')
+    console.log(res)
+    res.then(res => res.json())
+        .then(result => {
+            console.log(result);
+        }).catch(err => console.log(err))
+}
+btn6.addEventListener('click', fetchAllGundams)
+const fetchAllPilots = () => {
+    const res = fetch(url+'pilots')
+    console.log(res)
+    res.then(res => res.json())
+    .then(result => {
+        console.log(result);
+    }).catch(err => console.log(err))
+}
+btn7.addEventListener('click', fetchAllPilots)
