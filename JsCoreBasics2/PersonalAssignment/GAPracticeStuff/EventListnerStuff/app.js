4785eb0d58ce52c9f45fedff6cd56fcac4bf5036
mrylraihan <mr.ylraihan@gmail.com>
2022-05-17 14:20:03 -0700

    just incase

---
+++ JsCoreBasics2/PersonalAssignment/GAPracticeStuff/EventListnerStuff/app.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector('button')
+
+
+
+
+
+// we need locate the ul tag 
+const ul = document.querySelector('ul')
+// grab the lis from the ul 
+const listLi = ul.querySelectorAll('li')
+//index the right li
+console.log(listLi[4].innerText);
