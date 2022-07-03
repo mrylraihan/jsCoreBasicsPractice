ed5ae090e983669d14d6317ead0e0509af426daf
mrylraihan <mr.ylraihan@gmail.com>
2022-07-03 14:14:20 -0700

    5 object methods

--- JsCoreBasics2/PersonalAssignment/5ObjectFunctions/app.js
+++ JsCoreBasics2/PersonalAssignment/5ObjectFunctions/app.js
@@ -32,5 +32,83 @@ console.log(person2);//{name:'Susan'}
 const person3 = Object.assign({}, person,{name:'nike'})// by adding the empty object as the first parameter it will basically copy the previous object and override the previous values
 console.log(person);//{name: 'Susan'}
 console.log(person3);//{name: 'nike'}
+// this is now not modifying the exisiting object person
+// lets try again 
 
-//now lets say you dont want the same reference pointer 
+const cat = {name:'fluffy'}
+const cat2 = Object.assign({}, cat, {name:'fuzzy'})
+console.log(cat); //{ name: 'fluffy' }
+console.log(cat2);//{name: 'fuzzy'}
+
+//now the other way do it is with the spread operator 
+
+const cat3 = {...cat2, name:'Mittens'} 
+console.log(cat2);// { name: 'fuzzy' }
+console.log(cat3);//{name: 'Mittens'}
+
+
+
+//now lets say you don't want the same reference pointer 
+
+
+// now the next method is Object.defineProperty
+// now there 2 way to create an object with either an object literal 
+// const objName = {}
+// // or with a constructor
+// const objName = new ClassName()
+
+// now you can do it with this method
+// const table ={color:'brown'} 
+const table ={}
+Object.defineProperty(table, "color", {value:"brown"})
+console.log(table);//{color: 'brown'}
+// you can also make different preperties writable or not ,meaning they can or cant change the value
+const car = {}
+Object.defineProperty(car,"type",{value:'Tesla',writable:false} )//
+//now the type property has the writable value ot false, meaning we cant change it 
+console.log(car);//{type: 'Tesla'}
+car.type = 'BMW'//trying to change  the value but it wont work 
+console.log(car);//{type: 'Tesla'}
+
+
+//object.entries()
+//object.entries() takes an object and converts it to a array, depending on how many values it will become a multi dimensional array  
+const house = {firstFloor:'living room', secondFloor:'bed room'}
+
+console.log(house);//{firstFloor: 'living room', secondFloor: 'bed room'}
+const entriesHouse = Object.entries(house)
+console.log(entriesHouse);//[['firstFloor', 'living room'],['secondFloor', 'bed room']]
+
+
+//object.fromEntries is doing the reverse taking a multi-array and turning it to a object 
+//each array gets converted to a property in the object 
+//the first index will be the key and teh second the value of each array 
+// const arrPhone = [['brand', 'Apple']]//will become {brand:"Apple"}
+const arrPhone = [['brand', 'Apple']]//double nested arr
+const phone = Object.fromEntries(arrPhone)
+console.log(phone);//{brand: 'Apple'}
+const arrPhone2 = [['brand', 'Apple'],['model', 'iphone 13 pro']]//double nested arr
+const phone2 = Object.fromEntries(arrPhone2)
+console.log(phone);//{brand: 'Apple', model:'iphone 13 pro'}
+
+// Object.freeze is a method that freezes an object so you CanvasPattern, update, add, or delete properties from 
+
+const human = {name:'Wallie', age:30}
+Object.freeze(human)
+console.log(human);// {name:'Wallie', age:30}
+human.name = 'Wayel'//try to update 
+console.log(human);// {name:'Wallie', age:30}//doesnt change
+delete human.age //try to delete 
+console.log(human);// {name:'Wallie', age:30}//doesnt change
+human.occupation = 'developer' //try to delete 
+console.log(human);// {name:'Wallie', age:30}//doesnt change
+
+
+const human2 = { name: 'Wallie', age: 30 }
+console.log(human2);// {name:'Wallie', age:30}
+human2.name = 'Wayel'//try to update 
+console.log(human2);// {name: 'Wayel', age: 30}//changes
+delete human2.age //try to delete 
+console.log(human2);// {name: 'Wayel'}//removed property
+human2.occupation = 'developer' //try to delete 
+console.log(human2);// {name:'Wayel', occupation:'developer' }//
