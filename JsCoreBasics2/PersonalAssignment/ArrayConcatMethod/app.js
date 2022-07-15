fbfebf38ef0f6a7ef34f91c28c91f8541e471ec0
mrylraihan <mr.ylraihan@gmail.com>
2022-07-15 06:57:36 -0700

    array.concat

---
+++ JsCoreBasics2/PersonalAssignment/ArrayConcatMethod/app.js
@@ -0,0 +1,17 @@
+const arrayNamesObj = [
+    {name:'Wallie'},
+    {name:'Hisham'}
+]
+
+
+const p3 = {name:'Meshia'}
+
+const newArr = arrayNamesObj.concat(p3)
+console.log(newArr);
+console.log(arrayNamesObj);
+
+
+const newArrNums = [1,2,3]
+
+const combinedArr = arrayNamesObj.concat(newArrNums)
+console.log(combinedArr);
