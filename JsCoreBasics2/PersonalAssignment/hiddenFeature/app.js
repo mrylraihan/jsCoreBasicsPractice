1f2f333ae8de28ae4ba073014cb23a283da17aa4
mrylraihan <mr.ylraihan@gmail.com>
2023-04-15 07:13:28 -0700

    practice hiding and showing

--- JsCoreBasics2/PersonalAssignment/hiddenFeature/app.js
+++ JsCoreBasics2/PersonalAssignment/hiddenFeature/app.js
@@ -1,6 +1,16 @@
-const h1 = document.querySelector('h1')
-const btn = document.querySelector('button')
+const h1 = document.querySelectorAll('h1')
+const btn = document.querySelectorAll('button')
 
-btn.addEventListener('click', ()=>{
-    h1.toggleAttribute('hidden')
+btn[0].addEventListener('click', ()=>{
+    h1[0].toggleAttribute('hidden')
+})
+btn[1].addEventListener('click', ()=>{
+    if(!h1[1].hasAttribute('hidden')){
+        h1[1].setAttribute('hidden', '')
+    }else{
+        h1[1].removeAttribute('hidden')
+    }
+})
+btn[2].addEventListener('click', ()=>{
+   h1[2].classList.toggle('hide')
 })
