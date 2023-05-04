768b66b5ed18d879a886b35e726db37c58aa4e7c
mrylraihan <mr.ylraihan@gmail.com>
2023-05-04 15:57:30 -0400

    just incase

--- JsCoreBasics2/PersonalAssignment/ResponseObject/app.js
+++ JsCoreBasics2/PersonalAssignment/ResponseObject/app.js
@@ -1,7 +1,7 @@
 fetch('https://api.kanye.rest')
 .then(res=>res.json())
 .then(result=>{
-    console.log(result)
+    console.log(result.quote)
     const res = new Response({quote:'test'})
     console.log(res.quote)
 })
